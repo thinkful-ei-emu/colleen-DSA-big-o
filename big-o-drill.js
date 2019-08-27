768cf8cb7c11f31e7b14470ecbbd7141f83d4815
Colleen <colleenhiggins87@gmail.com>
2019-08-27 13:48:18 -0700

    additional answers through 14 added, not Hanoi yet

--- big-o-drill.js
+++ big-o-drill.js
@@ -168,7 +168,7 @@ step seven: 1 from A to C
 If you are given 5 disks, how do the rods look like after 7 recursive calls?
 How many moves are needed to complete the puzzle with 3 disks? with 4 disks? with 5 disks?
 What is the runtime of your algorithm? */
-
+/* 
 function tower(numDisk){
   for(let i=0; i < numDisk; i++){
     new Disk = {
@@ -177,22 +177,135 @@ function tower(numDisk){
       rod: 'a'
     }
   }
-}
+} */
 
 function tOH(numDisk, source, destination, temp){
+  
   //a to b
 if (numDisk === 1){
-  return {source, destination}
+  console.log({source, destination})
+  return
 }
+if(numDisk > 1){
 //reduce disk from 2 to one
-tOH(n-1, source, temp, dest)
-
+tOH(numDisk-1, source, temp, destination)
+//take care of the other one
+tOH(numDisk-1, source, destination, temp)
+//tOH()
 }
+}
+tOH(2, 'A', 'C', 'B')
+/* 
+tOH(2, A, C, B)
 12. Iterative version
 Solve the drills 1 - 7 from your previous checkpoint (Recursion) iteratively.
+1. */
+function howManySheep(n){
+  let count = n
+      while (count > 0){
+        console.log(`Another sheep jumped over the fence`)
+        count --
+      }
+      return 'All sheep jumped'
+  
+    }
+  //O(n)--dependent on number of sheep how many times while loop occurs
+  howManySheep(4)
 
+  //2. power calculator
+  function powerCalculator(b, e){
+    if (e < 0){
+      return 'exponent needs to be >=0'
+    }
+    else if(e ===0){
+      return 1
+    }
+    else {
+      let result = b
+
+      for (let i=1; i < e; i++ ){
+        result = result * result;
+      }
+      return result;
+    }
+    //O(n)--dependent on number of exponents how many times the for loop is run
+  }
+
+  //3. Reverse string
+
+  function stringReverse(string){
+    let arr = string.split('')
+
+    let reverseArr = arr.reverse()
+
+    let reverseString= reverseArr.join(',')
+    return reverseString
+  }
+//O(n)--dependent on length of string with methods being run
+
+//4. calculate triangular number == n(n+1)/2
+function triangularNumber(n){
+  let answer = n*(n+1)/2
+  return answer
+}
+//O(1)--basic math calculation doesn't depend on the size of the value in terms of steps needed 
+
+//5. splitter function
+function splitter(string, separator){
+  return string.split(separator)
+}
+
+function splitter(string, separator){
+  let filteredWord = ''
+
+  for(let i =0; i < string.length; i++){
+    if (string[i] !== separator){
+      filteredWord += string[i]
+    }
+    else filteredWord
+  }
+}
+//O(n)--run through loop length based on length of string
+
+//6. fibonacci
+function fib(n){
+  let sequence = [1, 1]
+  for (let i=2; i <= n; i++){
+    
+      sequence.push(sequence[i-1]+sequence[i-2])
+    
+  }
+  return sequence[n]
+}
+
+fib(7)
+//O(n)--run time for fn will be proportional to the number given to it because it will run through the length of the number to calculate the result
+//7. 
+function factorial(n){
+  //factorial--multiply that number by each number between itself and 1
+  let product = 1
+  for(let i=1; i <=n; i++){
+  product = i * product
+  }
+  return product
+  }
+  //O(n)--length of loop based on number, calculation does not change based on number input (so will take linear length to complete)
+  factorial(10)
+/* 
 13. Recursive Big O
 Take your solutions from the recursive exercises that you completed in the previous checkpoint and identify the time complexities (big O) of each of them.
+ */ 
+//1. howManySheep --O(n)--based on how many sheep are inputted
+//2. powerCalculator --O(n)--based on the exponent input runs fn that many times
+//3. stringReverse = O(n)--based on length of string, linear output
+//4. triangularNumber = O(n)--based on size of n, how many iterations it runs before it returns--linear
+//5. splitter = O(n)--based on length of string and presence of separator
+//6. fib= O(n)
+//7. factorial=O(n)
+//8. maze = O(n^X)-polynomial--dependent on position and options
+//or exponential
+//binary: O(log(n))--
 
-14. Iterative Big O
-Take your solutions from the iterative exercises today and identify the time complexities (big O) of each of them. */
+/* 14. Iterative Big O
+Take your solutions from the iterative exercises today and identify the time complexities (big O) of each of them. */ 
+//***** */see comments for iterative functions for answers
